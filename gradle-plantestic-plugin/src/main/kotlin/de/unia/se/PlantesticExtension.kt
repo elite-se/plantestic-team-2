8f0f4f11b0bd65702a095569c3a892747732c1b6
Maximilian Ammann <max@maxammann.org>
2020-07-16 18:25:07 +0200

    Simplify plugin usage

--- gradle-plantestic-plugin/src/main/kotlin/de/unia/se/PlantesticExtension.kt
+++ gradle-plantestic-plugin/src/main/kotlin/de/unia/se/PlantesticExtension.kt
@@ -1,18 +1,10 @@
 package de.unia.se
 
-import org.gradle.api.Action
 import org.gradle.api.tasks.SourceSet
 
 open class PlantesticExtension(val whenConfigAdded: (PlantesticExtension) -> SourceSet) {
 
-//    public var sourceSet: SourceSet? = null
-//
-//    open fun sourceSet(action: Action<in SourceSet?>) {
-//        action.execute(sourceSet)
-//        whenConfigAdded(this)
-//    }
-
-    public var sourceSet: SourceSet? = null
+    var sourceSet: SourceSet? = null
 
     open fun sourceSet(sourceSet : SourceSet) {
         this.sourceSet = sourceSet
--- gradle-plantestic-plugin/src/main/kotlin/de/unia/se/PlantesticPlugin.kt
+++ gradle-plantestic-plugin/src/main/kotlin/de/unia/se/PlantesticPlugin.kt
@@ -3,8 +3,11 @@ package de.unia.se
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.plugins.JavaBasePlugin
+import org.gradle.api.internal.provider.ValueSupplier.ValueProducer.task
 import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.api.tasks.testing.Test
+
 
 @Suppress("unused")
 class PlantesticPlugin : Plugin<Project> {
@@ -18,29 +21,42 @@ class PlantesticPlugin : Plugin<Project> {
         val plantesticRuntime = project.configurations.create("plantesticRuntime")
         plantesticRuntime.description = "The classpath used to invoke plantestic"
 
+        val outputDirectoryName = "${project.buildDir}/generated-src/plantestic/"
+
         project.extensions.create(
             PLANTESTIC_EXTENSION_NAME,
             PlantesticExtension::class.java,
             { extension: PlantesticExtension ->
-                val outputDirectoryName = "${project.buildDir}/generated-src/plantestic/"
+
 
                 val plantesticTask = project.tasks
-                    .create(PLANTESTIC_TASK_NAME, PlantesticTask::class.java, plantesticRuntime,
-                        outputDirectoryName, extension.sourceSet!!.resources)
+                    .create(
+                        PLANTESTIC_TASK_NAME, PlantesticTask::class.java, plantesticRuntime,
+                        outputDirectoryName, extension.sourceSet!!.resources
+                    )
                 plantesticTask.description = "Generates the plantestic testcases"
                 plantesticTask.group = "plantestic"
-
-
-                val sourceSet: SourceSet = extension.sourceSet!!
-                sourceSet.java.srcDir(plantesticTask)
             }
         )
 
 
+
+        project.plugins.apply("java")
+
+        val sourceSets = project.properties["sourceSets"] as SourceSetContainer?
+        val sourceSet = sourceSets!!.getByName("test")
+        sourceSet.java.srcDir(outputDirectoryName)
+
         addPlantesticDependencies(plantesticRuntime, project)
 
-        // Add "java" plugin
-        project.plugins.apply(JavaBasePlugin::class.java)
+        project.tasks.named("test", Test::class.java) {
+            it.useJUnitPlatform()
+            it.dependsOn("clean")
+            it.dependsOn("generate")
+        }
+
+        project.repositories.mavenCentral()
+        project.repositories.mavenLocal()
     }
 
     /**
@@ -58,5 +74,10 @@ class PlantesticPlugin : Plugin<Project> {
         }
 
         project.dependencies.add(plantesticRuntime.name, "de.unia.se:core:0.1")
+        project.dependencies.add("testImplementation", "io.rest-assured:rest-assured:3.0.0")
+        project.dependencies.add("testImplementation", "org.apache.commons:commons-text:1.7")
+        project.dependencies.add("testImplementation", "io.rest-assured:rest-assured:3.0.0")
+        project.dependencies.add("testImplementation", "javax.xml.bind:jaxb-api:2.3.1")
+        project.dependencies.add("testImplementation", "org.junit.jupiter:junit-jupiter-engine:5.3.1")
     }
 }
